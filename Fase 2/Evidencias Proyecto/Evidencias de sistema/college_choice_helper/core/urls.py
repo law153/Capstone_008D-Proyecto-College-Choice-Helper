96434aa762b93133db133083516f724cd3e9b8ae
Abel <abelx3678@gmail.com>
2025-09-12 18:20:58 -0300

    agregadas paginas login y registro

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/urls.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 from django.conf.urls import handler404
-from .views import mostrarIndex
+from .views import mostrarIndex, mostrarLogin, mostrarRegistro
 
 urlpatterns = [
     path('', mostrarIndex, name='index'),
+    path('inicio_sesion/', mostrarLogin, name='login'),
+    path('registro/', mostrarRegistro, name='registro'),
 ]
--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/views.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def mostrarIndex(request):
     return render(request, 'core/index.html')
+
+def mostrarLogin(request):
+    return render(request, 'core/sinCuenta/login.html')
+
+def mostrarRegistro(request):
+    return render(request, 'core/sinCuenta/registrarse.html')
+
