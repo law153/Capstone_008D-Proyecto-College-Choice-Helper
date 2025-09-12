5c5f95e11fb1696fdaec3aed0d9cddbf40ae9204
Abel <abelx3678@gmail.com>
2025-09-12 18:32:01 -0300

    creadas paginas de estudiante

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/urls.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path,include
 from django.conf.urls import handler404
-from .views import mostrarIndex, mostrarLogin, mostrarRegistro
+from .views import mostrarIndex, mostrarLogin, mostrarRegistro, mostrarRecomendaciones, mostrarVistaInstituciones, mostrarFormularioEstudiante
 
 urlpatterns = [
     path('', mostrarIndex, name='index'),
     path('inicio_sesion/', mostrarLogin, name='login'),
     path('registro/', mostrarRegistro, name='registro'),
+    path('formulario_estudiante/', mostrarFormularioEstudiante, name='formulario_estudiante'),
+    path('recomendaciones/', mostrarRecomendaciones, name='recomendaciones'),
+    path('ver_institucion/', mostrarVistaInstituciones, name='ver_instituciones'),
 ]
--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/views.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Sin cuenta
 def mostrarIndex(request):
     return render(request, 'core/index.html')
 
@@ -9,4 +9,18 @@ def mostrarLogin(request):
 
 def mostrarRegistro(request):
     return render(request, 'core/sinCuenta/registrarse.html')
+
+
+# Estudiantes
+def mostrarFormularioEstudiante(request):
+    return render(request, 'core/estudiantes/formularioEstudiante.html')
+
+def mostrarRecomendaciones(request):
+    return render(request, 'core/estudiantes/recomendaciones.html')
+
+def mostrarVistaInstituciones(request):
+    return render(request, 'core/estudiantes/vistaInstitucion.html')
+
+
+# Instituciones
 
