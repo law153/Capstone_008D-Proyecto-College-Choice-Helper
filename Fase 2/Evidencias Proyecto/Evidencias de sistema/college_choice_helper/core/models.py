8ea5af218d7848ad6fecd9f961a9317253ed19fe
Abel <abelx3678@gmail.com>
2025-09-15 18:51:51 -0300

    creados modelos de datos

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/models.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Rol(models.Model):
+    id_rol = models.IntegerField(primary_key=True, verbose_name='0 para estudiante y 1 para institución, quiza 2 para admin')
+    nombre_rol = models.CharField(max_length=30)
+    def __str__(self) -> str:
+        return self.nombre_rol
+    
+class Usuario(models.Model):
+    idUsuario = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    correo = models.CharField(max_length=50)
+    comunaUsuario = models.CharField(max_length=50)
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.correo
+
+class Parametros(models.Model):
+    idParametros = models.OneToOneField(Usuario, on_delete=models.CASCADE, primary_key=True)
+    comunaRelevancia = models.BooleanField(default=False)
+    budget = models.IntegerField(default=0)
+    gratuidad = models.BooleanField(default=False)
+    acreditacionDeseado = models.IntegerField(default=0)
+    esUniversidad = models.BooleanField(default=False)
+    puntajeNem = models.IntegerField(default=0)
+    carrera = models.CharField(max_length=100, default="")
+
+class Institucion(models.Model):
+    idInstitucion = models.AutoField(primary_key=True)
+    nombreInstitucion = models.CharField(max_length=100)
+    comunaInstitucion = models.CharField(max_length=50)
+    esUniversidadInsti = models.BooleanField(default=False) 
+    costo = models.IntegerField()
+    adscritoGratuidad = models.BooleanField(default=False)
+    acreditacion = models.IntegerField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.nombreInstitucion
+    
+class Carrera(models.Model):
+    idCarrera = models.AutoField(primary_key=True)
+    nombreCarrera = models.CharField(max_length=100)
+    puntajeMinimo = models.IntegerField()
+    institucion = models.ForeignKey(Institucion, on_delete=models.CASCADE)
