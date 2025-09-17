c4b9fd611273de1fefc87434742d1e8809fc3333
Abel <abelx3678@gmail.com>
2025-09-17 17:51:15 -0300

    pequeno alter en modelo de institucion
    + admin de django

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/admin.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Rol, Usuario, Parametros, Institucion, Carrera
 # Register your models here.
+
+admin.site.register(Rol)
+admin.site.register(Usuario)
+admin.site.register(Parametros)
+admin.site.register(Institucion)
+admin.site.register(Carrera)
