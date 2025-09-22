4b07eb89ea8d6c2d2489042ca65d26470f870317
alexignahdz <al.hernandezo@duocuc.cl>
2025-09-22 18:05:38 -0300

    Separación de paths en urls y se agregaron ambos paths de gestion

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/urls.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/urls.py
@@ -3,18 +3,25 @@ from django.conf.urls import handler404
 from .views import mostrarIndex, mostrarLogin, mostrarRegistro, mostrarOlvidoClave
 from .viewsEstu import mostrarFormularioEstudiante, mostrarRecomendaciones, mostrarVistaInstituciones, mostrarCambioClave, mostrarCambioCorreo, mostrarGestionCuenta
 from .viewsInsti import mostrarRegistroInstitucion, mostrarEditarInstitucion
+from .viewsAdmin import mostrarGestionEstu, mostrarGestionInsti
 
 urlpatterns = [
+    #SinCuenta
     path('', mostrarIndex, name='index'),
     path('inicio_sesion/', mostrarLogin, name='login'),
     path('registro/', mostrarRegistro, name='registro'),
+    path('olvide_clave/', mostrarOlvidoClave, name="olvide_clave"),
+    #Estudiante
     path('formulario_estudiante/', mostrarFormularioEstudiante, name='formulario_estudiante'),
     path('recomendaciones/', mostrarRecomendaciones, name='recomendaciones'),
     path('ver_institucion/', mostrarVistaInstituciones, name='ver_instituciones'),
-    path('agregar_institucion/', mostrarRegistroInstitucion, name="agregar_institucion"),
-    path('editar_institucion/', mostrarEditarInstitucion, name="editar_institucion"),
     path('cambio_clave/', mostrarCambioClave, name="cambio_clave"),
     path('cambio_correo/', mostrarCambioCorreo, name="cambio_correo"),
     path('gestion_cuenta/', mostrarGestionCuenta, name="gestion_cuenta"),
-    path('olvide_clave/', mostrarOlvidoClave, name="olvide_clave"),
+    #Instituciones
+    path('agregar_institucion/', mostrarRegistroInstitucion, name="agregar_institucion"),
+    path('editar_institucion/', mostrarEditarInstitucion, name="editar_institucion"),
+    #Admin
+    path('gestionar_estu/', mostrarGestionEstu, name="gestionar_estu"),
+    path('gestionar_insti/', mostrarGestionInsti, name="gestionar_insti"),
 ]
