1b639bc89e24c4e498f35cfb88c3f405d271e37f
Abel <abelx3678@gmail.com>
2025-09-25 19:21:39 -0300

    Trabajando peticiones
    + puestos links

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/viewsAdmin.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/viewsAdmin.py
@@ -1,28 +1,35 @@
 from django.shortcuts import render
+from .models import Usuario, Rol, Peticiones, Institucion
 
 #Admin
 def mostrarGestionEstu(request):
     rol = request.session.get('rol', None)
+    correo = request.session.get('correo', None)
 
-    contexto = {'rol': rol}
+    contexto = {'rol': rol, 'correo': correo}
     return render (request,'core/admin/gestionEstudiantes.html', contexto)
 
 def mostrarGestionInsti(request):
     rol = request.session.get('rol', None)
-
-    contexto = {'rol': rol}
+    correo = request.session.get('correo', None)
+    contexto = {'rol': rol, 'correo': correo}
     return render (request,'core/admin/gestionInstituciones.html', contexto)
 
 def mostrarVerPeticiones(request):
     rol = request.session.get('rol', None)
+    correo = request.session.get('correo', None)
 
-    contexto = {'rol': rol}
+    peticionesRol = Peticiones.objects.filter(tipoPeticion="Cambio de rol")
+    peticionesGeneral = Peticiones.objects.exclude(tipoPeticion="Cambio de rol")
+
+    contexto = {'rol': rol, 'correo': correo, 'peticionesRol': peticionesRol, 'peticionesGeneral': peticionesGeneral}
     return render (request,'core/admin/verPeticiones.html', contexto)
 
 def mostrarVerPeticion(request):
     rol = request.session.get('rol', None)
+    correo = request.session.get('correo', None)
 
-    contexto = {'rol': rol}
+    contexto = {'rol': rol, 'correo': correo}
 
     return render (request,'core/admin/verPeticion.html', contexto)
 
