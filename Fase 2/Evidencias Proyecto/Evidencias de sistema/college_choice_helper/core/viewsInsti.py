b4333a4771096322e0b1bfbde0a3d74849bb2d32
Abel <abelx3678@gmail.com>
2025-10-10 16:48:17 -0300

    pequeño fix en editar institucion

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/viewsInsti.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/college_choice_helper/core/viewsInsti.py
@@ -177,11 +177,7 @@ def actualizarInsti(request):
             institucion = Institucion.objects.get(idInstitucion = idI)
             fotoI = request.FILES.get('foto_insti', institucion.fotoInstitucion)
 
-            existeInsti = Institucion.objects.filter(
-                nombreInstitucion = nombreI
-            ).first()
-
-            if existeInsti:
+            if Institucion.objects.filter(nombreInstitucion=nombreI.strip()).exclude(idInstitucion=idI).exists():
                 messages.error(request,"Ya hay una institución con el mismo nombre")
                 return redirect('mostrarEditarInstitucion', idI)
             else:
