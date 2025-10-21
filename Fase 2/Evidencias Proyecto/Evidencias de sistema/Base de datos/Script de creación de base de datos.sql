62dcb175dad1c597618b565d2e3a912f895a90fe
Abel <abelx3678@gmail.com>
2025-10-21 12:49:15 -0300

    agregados scripts de database

---
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Base de datos/Script de creación de base de datos.sql
@@ -0,0 +1,14 @@
+-- Database: choiceHelper
+
+-- DROP DATABASE IF EXISTS "choiceHelper";
+
+CREATE DATABASE "choiceHelper"
+    WITH
+    OWNER = "usuarioFuture"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Chile.1252'
+    LC_CTYPE = 'Spanish_Chile.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
